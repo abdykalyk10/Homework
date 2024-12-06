58296debbcabff04ee729de280e5c82ee76f253c
abdykalyk10 <abdykalyk25>
2024-12-06 15:52:03 +0600

    lesson4

--- Geeks/lesson2_2.py
+++ Geeks/lesson2_2.py
@@ -32,9 +32,9 @@ if __name__ == "__main__":
     square1 = Square(5)
     square2 = Square(7)
 
-    rectangle1 = Rectangle(5, 10)
-    rectangle2 = Rectangle(2, 4)
-    rectangle3 = Rectangle(6, 8)
+    rectangle1 = Rectangle(7, 10)
+    rectangle2 = Rectangle(3, 4)
+    rectangle3 = Rectangle(5, 8)
 
     figures = [square1, square2, rectangle1, rectangle2, rectangle3]
 
